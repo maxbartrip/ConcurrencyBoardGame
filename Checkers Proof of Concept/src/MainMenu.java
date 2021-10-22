c071382eed536ab1a06c155572663da96238925d
maxbartrip <maximillianbartrip@yahoo.co.uk>
2021-10-22 18:35:55 +0100

    Created a basic menu with buttons for each game type

--- Checkers Proof of Concept/src/MainMenu.java
+++ Checkers Proof of Concept/src/MainMenu.java
@@ -1,4 +1,11 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MainMenu extends Application {
@@ -6,6 +13,29 @@ public class MainMenu extends Application {
   @Override
   public void start(Stage stage) throws Exception {
     stage.setTitle("Main Menu");
+    
+    Text menuTitle = new Text();
+    menuTitle.setFont(Font.font(null, 20));
+    menuTitle.setText("Please choose a game to play");
+    
+    Button checkersBtn = new Button("Play Checkers");
+    Button chessBtn = new Button("Play Chess");
+    chessBtn.setDisable(true);
+    Button draughtsBtn = new Button("Play Polish Draughts");
+    draughtsBtn.setDisable(true);
+    Button gomokuBtn = new Button("Play Gomoku");
+    gomokuBtn.setDisable(true);
+    Button damaBtn = new Button("Play Dama");
+    damaBtn.setDisable(true);
+    
+    
+    
+    HBox buttonLayout = new HBox(20, checkersBtn, chessBtn, draughtsBtn, gomokuBtn, damaBtn);
+    buttonLayout.setAlignment(Pos.CENTER);
+    VBox menuLayout = new VBox(20, menuTitle, buttonLayout);
+    menuLayout.setAlignment(Pos.CENTER);
+    Scene menuScene = new Scene(menuLayout, 600, 600);
+    stage.setScene(menuScene);
     stage.show();
   }
   
