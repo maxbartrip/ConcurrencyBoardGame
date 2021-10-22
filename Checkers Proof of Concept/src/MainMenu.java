0ee82ca9d6ba7ba426928bb794b4fa96d75d5e9f
maxbartrip <maximillianbartrip@yahoo.co.uk>
2021-10-22 18:07:59 +0100

    Added JavaFX and created class for main menu interface

---
+++ Checkers Proof of Concept/src/MainMenu.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainMenu extends Application {
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    stage.setTitle("Main Menu");
+    stage.show();
+  }
+  
+  public static void main(String[] args) {
+    launch(args);
+  }
+  
+}
