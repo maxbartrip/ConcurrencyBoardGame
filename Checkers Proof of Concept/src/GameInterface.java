68ce06a508ba3c526664b276419d3a72cf34ee5a
maxbartrip <maximillianbartrip@yahoo.co.uk>
2021-10-27 15:53:08 +0100

    added javadoc

--- Checkers Proof of Concept/src/GameInterface.java
+++ Checkers Proof of Concept/src/GameInterface.java
@@ -2,10 +2,21 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 
+/**
+ * @author Max
+ * 
+ * This class is used to create the board that can be displayed to the user and to create the visual display of the pieces on that board.
+ *
+ */
 public class GameInterface {
   
   private static Scene gameScene;
 
+  /**
+   * This constructor takes a gameBoard and creates a visual board that can be displayed on a javaFX interface, based on the board that has been input.
+   * 
+   * @param gameBoard the gameBoard of the current game so that the board interface can be initialised
+   */
   public GameInterface(Board gameBoard) {
     //InputStream stream = new FileInputStream("src/resources/CHECKERS_BLACK_KING.png");
     //Image checkersPiece = new Image(stream);
@@ -34,11 +45,16 @@ public class GameInterface {
     this.gameScene = new Scene(gamePane);
   }
   
+  /**
+   * This method is used to return the Scene to the javaFX class, so that it can be displayed to the user.
+   * 
+   * @return the Scene gameScene which shows the board and pieces.
+   */
   public static Scene getScene() {
     return gameScene;
   }
   
-  public void updateBoard() {
+  public void updateBoard(Board gameBoard) {
     
   }
 }
