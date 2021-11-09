6a36aaa6d491820cd9de7f87fd943f462df0ab74
maxbartrip <maximillianbartrip@yahoo.co.uk>
2021-11-09 13:20:07 +0000

    Attempt to resolve issue #5 using threads, doesn't work properly

--- Checkers Proof of Concept/src/MainMenu.java
+++ Checkers Proof of Concept/src/MainMenu.java
@@ -84,6 +84,9 @@ public class MainMenu extends Application {
           newStage.setScene(gameScene);
           newStage.setResizable(false);
           newStage.show();
+          GameOverThread gameOver = new GameOverThread(gameBoard, newStage);
+          gameOver.setDaemon(true);
+          gameOver.start();
         }
         else {
           Alert plsSelect = new Alert(AlertType.WARNING);
