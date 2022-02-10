708f981bc14506442c1e24c5b26baa4dd2298868
maxbartrip <maximillianbartrip@yahoo.co.uk>
2022-02-10 01:55:12 +0000

    Added files for Server proof of concept program
    This program displays an application that creates an interface and a
    server or client allowing the user to send messages between the client
    and server with new messages being displayed on the interface.

---
+++ Server Proof of Concept/src/ChatApp.java
@@ -0,0 +1,97 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * @author Max
+ * 
+ * This class creates a chat window which allows the user to send messages between a client and a server.
+ * 
+ * This program is partially based on: https://github.com/AlmasB/FXTutorials/tree/ebc1d95f2dee01e28b84efc14ef9f1b0102bf8f2/src/main/java/com/almasb/chat
+ *
+ */
+public class ChatApp extends Application {
+  
+  private boolean isServer = true;
+  /* This value can be modified to change whether a server or a client is created. When set to true it will create a server,
+   * when set to false it will create a client that connects to that server. A server must already be running for a client to connect.
+  */
+  
+  private TextArea messages = new TextArea();
+  private NetworkConnection connection = isServer ? new Server(3001, messages) : new Client("127.0.0.1", 3001, messages);
+  // If the connection isServer is true, connection is a new Server(), if it is false, connection is a new Client().
+  
+  /**
+   * This method starts the NetworkConnection and creates and displays the interface that allows the user to send messages and see received messages.
+   * 
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    connection.startConnection();
+    // Start the server/client
+    
+    String title;
+    if (isServer) {
+      title = "Chat App: Server";
+    }
+    else {
+      title = "Chat App: Client";
+    }
+    
+    primaryStage.setTitle(title);
+    
+    messages.setEditable(false);
+    messages.setPrefHeight(550);
+    
+    TextField input = new TextField();
+    
+    // When enter is pressed this event will activate
+    input.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        String message = input.getText();
+        input.clear();
+        messages.appendText(message+"\n");
+        
+        try {
+          connection.send(message);
+        }
+        catch (Exception e) {
+          messages.appendText("Error: Failed to send message\n");
+          e.printStackTrace();
+        }
+      }
+      
+    });
+    
+    VBox chatLayout = new VBox(20, messages, input);
+    chatLayout.setPrefSize(600, 600);
+    Scene primaryScene = new Scene(chatLayout);
+    primaryStage.setScene(primaryScene);
+    primaryStage.show();
+    input.requestFocus();
+  }
+  
+  /**
+   * This method closes the NetworkConnection when the interface is closed.
+   */
+  @Override
+  public void stop() throws Exception {
+    connection.closeConnection();
+  }
+
+  /**
+   * This is the main method which launches the interface, running the start method.
+   * 
+   * @param args the command line arguments.
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+}
---
+++ Server Proof of Concept/src/Client.java
@@ -0,0 +1,51 @@
+import javafx.scene.control.TextArea;
+
+/**
+ * @author Max
+ * 
+ * This class is an extension of NetworkConnection for when a Client is to be created.
+ *
+ */
+public class Client extends NetworkConnection {
+  
+  private String ip;
+  private int port;
+  
+  /**
+   * This is the constructor for the Client class. It first runs the constructor of NetworkConnection, passing the TextArea from the interface to the NetworkConnection constructor.
+   * 
+   * @param ip the String ip address of the server that is to be connected to
+   * @param port the Integer port of the server that is to be connected to
+   * @param messages the TextArea on the interface where messages are to be displayed
+   */
+  public Client(String ip, int port, TextArea messages) {
+    super(messages);
+    this.ip = ip;
+    this.port = port;
+  }
+
+  /**
+   * This method is a check to see if the NetworkConnection is a server or not, as this is a Client class it returns false.
+   */
+  @Override
+  protected Boolean isServer() {
+    return false;
+  }
+
+  /**
+   * This method returns the IP of the server the client is to connect to.
+   */
+  @Override
+  protected String getIP() {
+    return ip;
+  }
+
+  /**
+   * This method returns the port of the server that is to be connected to.
+   */
+  @Override
+  protected int getPort() {
+    return port;
+  }
+  
+}
