5086dead72412ef08f9f20f06f51a31c077ca291
maxbartrip <maximillianbartrip@yahoo.co.uk>
2022-02-11 22:01:33 +0000

    Added files for Concurrency Based Board Game
    This is the start of the complete concurrency based board game, with
    redesigned parts. These files include a new enum Colour and a modified
    version of the Piece class.

---
+++ Concurrency Based Board Game/src/Piece.java
@@ -0,0 +1,92 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+/**
+ * @author Max
+ * 
+ * This is the object that will store information about a game piece such as the type, position on the board colour etc.
+ *
+ */
+public class Piece {
+  private Type type;
+  private int xPos;
+  private int yPos;
+  private Colour colour;
+  
+  /**
+   * This is the constructor which sets the information about the piece.
+   * 
+   * @param type The enum type that the piece is, this describes the game that the piece is from and it's type such as man, king, queen etc.
+   * @param xPos The integer position along the x axis of the board that the piece lies.
+   * @param yPos The integer position along the y axis of the board that the piece lies.
+   * @param colour The enum colour of the piece, signifying which team the piece belongs to.
+   */
+  public Piece(Type type, int xPos, int yPos, Colour colour) {
+    this.type = type;
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.colour = colour;
+  }
+  
+  public Type getType() {
+    return this.type;
+  }
+  
+  public int getxPos() {
+    return this.xPos;
+  }
+  
+  public int getyPos() {
+    return this.yPos;
+  }
+  
+  public Colour getColour() {
+    return this.colour;
+  }
+  
+  /**
+   * This is a setter method will be needed for promoting pieces during a game i.e. in checkers a man may become a king.
+   * 
+   * @param newType the enum Type for the new type of the piece
+   */
+  public void setType(Type newType) {
+    this.type = newType;
+  }
+  
+  /**
+   * This is a setter for updating the position of the piece.
+   * 
+   * @param newXPos the integer of the new position along the x axis of the piece.
+   */
+  public void setXPos(int newXPos) {
+    this.xPos = newXPos;
+  }
+  
+  /**
+   * This is a setter for updating the position of the piece.
+   * 
+   * @param newYPos the integer of the new position along the y axis of the piece.
+   */
+  public void setYPos(int newYPos) {
+    this.yPos = newYPos;
+  }
+  
+  /**
+   * This method returns the InputStream for the location of the image corresponding to this piece.
+   * 
+   * @return the InputStream for the location of the image corresponding to the piece.
+   * @throws FileNotFoundException if the location of the image cannot be found an appropriate exception is thrown.
+   */
+  public InputStream getImage() throws Exception {
+    InputStream stream = null;
+    String location = "src/resources/"+this.type+"_"+this.colour.toString()+".png";
+    try {
+      stream = new FileInputStream(location);
+    }
+    catch(FileNotFoundException e) {
+      throw new FileNotFoundException("ERROR: Image cannot be located.");
+    }
+    return stream;
+  }  
+}
---
+++ Concurrency Based Board Game/src/Type.java
@@ -0,0 +1,12 @@
+
+/**
+ * @author Max
+ * 
+ * This is the enum that signifies the game and type of a piece. More types will be added to this for each different game e.g. chess will have CHESS_PAWN etc.
+ * This will be used to check legal moves of a piece based on which game and which sprite to display on the board.
+ *
+ */
+public enum Type {
+  CHECKERS_MAN,
+  CHECKERS_KING;
+}
