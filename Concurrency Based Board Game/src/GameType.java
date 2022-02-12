7640431c9c7724da838c31e80b015268af5a7562
maxbartrip <maximillianbartrip@yahoo.co.uk>
2022-02-12 22:56:47 +0000

    Added Tile and GameType

---
+++ Concurrency Based Board Game/src/GameType.java
@@ -0,0 +1,15 @@
+
+/**
+ * @author Max
+ * 
+ * This is the enum that signifies which type of game the user is playing. This is important as each game will have different rules and potentially a different board.
+ * This is used to create the correct size board for each game.
+ *
+ */
+public enum GameType {
+  CHECKERS,
+  CHESS,
+  DRAUGHTS,
+  GOMOKU,
+  DAMA;
+}
---
+++ Concurrency Based Board Game/src/Tile.java
@@ -0,0 +1,26 @@
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Tile extends Rectangle {
+  
+  private int boardX;
+  private int boardY;
+  
+  public Tile(boolean dark, int boardX, int boardY) {
+    setWidth(64);
+    setHeight(64);
+    setFill(dark ? Color.BEIGE : Color.DARKSEAGREEN);
+    setX(boardX*64);
+    setY(boardY*64);
+    this.boardX = boardX;
+    this.boardY = boardY;
+  }
+  
+  public int getBoardX() {
+    return this.boardX;
+  }
+  
+  public int getBoardY() {
+    return this.boardY;
+  }
+}
