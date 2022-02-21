b1c93b64648f53140f17c146dabceba280bd69e0
maxbartrip <maximillianbartrip@yahoo.co.uk>
2022-02-21 17:36:54 +0000

    Added check if piece has reached promotion zone

--- Concurrency Based Board Game/src/Board.java
+++ Concurrency Based Board Game/src/Board.java
@@ -205,6 +205,10 @@ public class Board {
     else {
       System.out.println("That move is illegal.");
     }
+    
+    if((currentPiece.getColour() == Colour.WHITE && newY == 7) || (currentPiece.getColour() == Colour.BLACK && newY == 0)) {
+      currentPiece.setType(Type.CHECKERS_KING);
+    }
   }
   
   /**
