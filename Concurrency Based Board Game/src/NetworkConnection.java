2d017b5056887f154d8802c71bb66e0321ec33d2
maxbartrip <maximillianbartrip@yahoo.co.uk>
2022-02-24 22:56:26 +0000

    Added classes for server and client setup to game

---
+++ Concurrency Based Board Game/src/NetworkConnection.java
@@ -0,0 +1,109 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javafx.application.Platform;
+import javafx.scene.control.TextArea;
+
+public abstract class NetworkConnection {
+  private ConnectionThread connThread;
+  
+  /**
+   * The constructor method for NetworkConnection, creates a ConnectionThread with the TextArea that is passed to it.
+   * 
+   * @param messages the TextArea on the interface where messages are displayed
+   */
+  public NetworkConnection(TextArea messages) {
+    connThread = new ConnectionThread(messages);
+    connThread.setDaemon(true);
+  }
+  
+  /**
+   * Starts the ConnectionThread, calls the run() method.
+   * 
+   * @throws Exception IllegalThreadStateException if the thread was already started.
+   */
+  public void startConnection() throws Exception {
+    connThread.start();
+  }
+  
+  /**
+   * This method takes a String message and writes it to the DataOutputStream of the ConnectionThread allowing it to send messages to the Server/Client
+   * 
+   * @param message the String message that is to be sent
+   * @throws Exception
+   */
+  public void send(String message) throws Exception {
+    connThread.output.writeUTF(message);
+  }
+  
+  /**
+   * This method closes the socket of the connectionThread allowing the socket to safely close.
+   * 
+   * @throws Exception
+   */
+  public void closeConnection() throws Exception {
+    connThread.socket.close();
+  }
+  
+  protected abstract Boolean isServer();
+  protected abstract String getIP();
+  protected abstract int getPort();
+  
+  /**
+   * @author Max
+   *
+   * This class is a thread that allows the program to constantly check for messages whilst the interface is running as well as interact with the interface to display messages.
+   */
+  private class ConnectionThread extends Thread {
+    private Socket socket;
+    private DataOutputStream output;
+    private TextArea messages;
+    
+    /**
+     * This is the constructor for the ConnectionThread class, setting messages to TextArea where messages are displayed so that the thread can interact with it.
+     * 
+     * @param messages the TextArea where messages are to displayed
+     */
+    ConnectionThread(TextArea messages) {
+      this.messages = messages;
+    }
+    
+    /**
+     * This method contains the functionality of the ConnectionThread class, which creates a ServerSocket and or Socket depending on whether the NetworkConnection is a server or not.
+     * It then creates the DataOutputStream and DataInputStream. It then starts a loop that is constantly checking for messages being received and appends them to the TextArea.
+     *
+     */
+    @Override
+    public void run() {
+      try {
+        
+        if(isServer()) {
+          ServerSocket server = new ServerSocket(getPort());
+          socket = server.accept();
+        }
+        else {
+          socket = new Socket(getIP(), getPort());
+        }
+        
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        DataInputStream input = new DataInputStream(socket.getInputStream());
+        
+        this.socket = socket;
+        this.output = output;
+        socket.setTcpNoDelay(true);
+        
+        while (true) {
+          String message = input.readUTF();
+          Platform.runLater(() -> {
+            messages.appendText(message+"\n");
+          });
+        }
+      }
+      catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
---
+++ Concurrency Based Board Game/src/Server.java
@@ -0,0 +1,48 @@
+import javafx.scene.control.TextArea;
+
+/**
+ * @author Max
+ * 
+ * This class is an extension of NetworkConnection for when a Server is to be created.
+ *
+ */
+public class Server extends NetworkConnection {
+  
+  private int port;
+  
+  /**
+   * This is the constructor for the Server class. It first runs the constructor of NetworkConnection, passing the TextArea from the interface to the NetworkConnection constructor.
+   * 
+   * @param port the Integer port of the server to be created
+   * @param messages the TextArea on the interface where messages are to be displayed
+   */
+  public Server(int port, TextArea messages) {
+    super(messages);
+    this.port = port;
+  }
+  
+  /**
+   * This method is a check to see if the NetworkConnection is a server or not, as this is a Server class it returns true.
+   */
+  @Override
+  protected Boolean isServer() {
+    return true;
+  }
+
+  /**
+   * This method returns the IP of the server the client is to connect to, as this is a server it is null.
+   */
+  @Override
+  protected String getIP() {
+    return null;
+  }
+
+  /**
+   * This method returns the port of the server that is to be created, so that the client can connect to it.
+   */
+  @Override
+  protected int getPort() {
+    return port;
+  }
+
+}
